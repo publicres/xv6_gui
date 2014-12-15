fb73cc5746dcceaab4a436ef3bbbb7ef89ad0c2d
Levy <zly.george@163.com>
2014-12-15 18:37:41 +0800

    增加显卡初始化函数以及在指定像素画点的函数

--- graphbase.c
+++ graphbase.c
@@ -3,21 +3,37 @@
 #include "memlayout.h"
 #include "graphbase.h"
 
-uint GraphicMem=KERNBASE+0x1028;
+uchar *base;
+
+void initGraphMode()
+{
+    uint GraphicMem=KERNBASE+0x1028;
+    uint baseAdd=*((uint*)GraphicMem);
+    base=(uchar*)baseAdd;
+    WIDTH_RES =*((ushort*)(KERNBASE+0x1012));
+    HEIGHT_RES =*((ushort*)(KERNBASE+0x1014));
+
+    cprintf("@Resolution Width: %d\n", WIDTH_RES);
+    cprintf("@Resolution Height:  %d\n", HEIGHT_RES);
+    cprintf("@Bits per pixel:    %d\n",*((uchar*)(KERNBASE+0x1019)));
+
+    cprintf("@Video card drivers initialized successfully.\n");
+}
+
+void setPixelColor(uint x, uint y, uchar r, uchar g, uchar b)
+{
+    uint pos=(y*WIDTH_RES+x)*3;
+    uchar *add=base+pos;
+    *add=b;
+    *(++add)=g;
+    *(++add)=r;
+}
 
 void tryToWrite()
 {
-    uint baseAdd=*((uint*)GraphicMem);
-    uchar *base=(uchar*)baseAdd;
-    cprintf("Bits per pixel: %x\n",*((uchar*)(KERNBASE+0x1019)));
-    uint x,y;
-    for (x=0;x<WIDTH_RES;x++)
-        for (y=0;y<HEIGHT_RES;y++)
-        {
-            base[0]=0xFF;
-            base[1]=0x00;
-            base[2]=0x00;
-            base+=3;
-        }
+    uint i,j;
 
+    for (j=0;j<HEIGHT_RES;j++)
+        for (i=0;i<WIDTH_RES;i++)
+            setPixelColor(i,j,i,i>>8,j);
 }
--- graphbase.h
+++ graphbase.h
@@ -1,5 +1,7 @@
 
-#define WIDTH_RES 1024
-#define HEIGHT_RES 768
+ushort WIDTH_RES;
+ushort HEIGHT_RES;
 
 void tryToWrite();
+void initGraphMode();   //Invoke it before anyother graphbase function.
+void setPixelColor(uint x, uint y, uchar r, uchar g, uchar b);
