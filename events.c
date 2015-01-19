a140771af1825e0f248648196ec5bac05d34faa3
BaoYu <fairytale12345@163.com>
2015-01-19 12:37:48 +0800

    鼠标进进出出~~

--- events.c
+++ events.c
@@ -8,6 +8,9 @@
 #include "message.h"
 #include "mouse.h"
 
+uint mouse_within_dom = -1;
+
+//We must judge whether our mouse has moved out of the dom, because the coordinates may out of range
 uchar typicalPointEvent(dom* which ,uint x,uint y,uint msgType)
 {
     if (which->pid==-1)
@@ -21,7 +24,29 @@ uchar typicalPointEvent(dom* which ,uint x,uint y,uint msgType)
     if (msgType & LEFT_BTN_UP)
         setABSFocus(which);
 
-    enqueue(which->pid, mm);
+    if (mm->dom_id != mouse_within_dom) //move out
+    {
+        if (mouse_within_dom != -1)
+        {
+            dom* tmp = (dom*)mouse_within_dom;  //move out from this dom
+            MouseMsg* mm_old = (MouseMsg*)kalloc();
+            mm_old->msg_type = MOUSE_MESSAGE;
+            mm_old->x = x;
+            mm_old->y = y;
+            mm_old->mouse_event_type = msgType;
+            mm_old->dom_id = mouse_within_dom;
+            mm_old->enter_or_leave = MOUSE_LEAVE;
+            enqueue(tmp->pid, mm_old);
+        }
+        mouse_within_dom = which->_id;
+        mm->enter_or_leave = MOUSE_ENTER;
+        enqueue(which->pid, mm);
+    }
+    else if ((msgType | 0x0) != 0)  //click buton(s)
+    {
+        mm->enter_or_leave = MOUSE_NOT_CHANGE;
+        enqueue(which->pid, mm);
+    }
     return 1;
 }
 
